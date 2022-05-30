e1981a0821effd6024ae5334ef222d1d927d8be3
Sam Gorinsky <sgorinsky@gmail.com>
2022-05-30 17:29:11 -0400

    logging in install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "INSTALLING..."
 easy_install pip
+echo "pip installed!"
 export POETRY_VERSION=1.1.8
 pip install "poetry==$POETRY_VERSION"
+echo "poetry installed!"
 cd /home/ec2-user/page-turn-decipher
+echo "now at page-turn-decipher: $(pwd)"
 pip install -r requirements.txt
+echo "deps installed!"
